aa89e7aa644af617b0db6dd1e281a7c2e24f08c3
dlsso <supertonesfan@hotmail.com>
2014-07-21 11:35:15 -0600

    Finished intro

--- read.js
+++ read.js
@@ -1,1 +1,9 @@
 var fs = require('fs')
+
+// This will read the content of whatever file you specify
+// process.argv[2] grabs the first argument after node and executing file
+var textContent = fs.readFileSync(process.argv[2], 'utf-8')
+
+
+console.log(textContent)
+
